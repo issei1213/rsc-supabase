700642544e4637c6cb1e22ed7310e7b36a3c65c3
issei1213 <59830008+issei1213@users.noreply.github.com>
2023-08-04 19:19:12 +0900

    [update] 12.Server component and client component

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+https://github.com/GomaGoma676/nextjs-app-router-supabase
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import NotesList from "@/app/components/notes-list";
+import TimerCounter from "@/app/components/time-counter";
+import {Suspense} from "react";
+import Spinner from "@/app/components/spinner";
+import RefreshBtn from "@/app/components/refresh-btn";
 
 export default function Page() {
   return (
       <main>
         <div className='m-10 text-center'>
             <p>Hello World🚀</p>
-            <NotesList />
+            <Suspense fallback={<Spinner color='border-green-500'/>}>
+                <NotesList />
+            </Suspense>
+            <TimerCounter />
+            <RefreshBtn />
         </div>
       </main>
   )
