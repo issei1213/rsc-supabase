92925808aa923ff698aca17dc15615e9e19e5f4b
issei1213 <59830008+issei1213@users.noreply.github.com>
2023-08-04 19:33:46 +0900

    [update] 13.Nested layout

---
+++ app/nested-layout/second/third/layout.tsx
@@ -0,0 +1,10 @@
+export default function ThirdLayout({ children }: {
+    children: React.ReactNode
+}) {
+    return (
+        <>
+            <p>Layout 3</p>
+            {children}
+        </>
+    )
+}
